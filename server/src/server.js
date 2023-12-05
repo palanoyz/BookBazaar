6ae1e956b181201d24d0c1bbab7766e35a7a3c92
Ariez <kritamet.cho@gmail.com>
2023-12-05 20:25:36 +0700

    update signup & login

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,8 @@ const { MongoClient, ObjectId } = require('mongodb')
 
 const PORT = 5000
 const app = express()
+const url = "mongodb+srv://kritamet:1234@cluster0.ebbbwuy.mongodb.net/"
+const client = new MongoClient(url)
 
 app.use(express.json())
 app.use(cors({
@@ -19,27 +21,70 @@ app.listen(PORT, async () => {
     console.log(`Server started at port ${PORT}`)
 })
 
+const connectDB = async () => {
+    try {
+        await client.connect();
+        console.log("Connected to DB");
+    } catch (e) {
+        console.log("Error", e);
+    }
+};
+
 app.get('/', (req,res)=>{
-    res.send({ message: 'home' })
+    res.send({ message: 'BookBazaar' })
 })
 
-const url = "mongodb+srv://kritamet:1234@cluster0.ebbbwuy.mongodb.net/"
-const client = new MongoClient(url)
-
+// Sign Up
 app.post('/api/signup', async (req,res)=>{
     try {
-        const {username, email, password} = req.body
-        const passwordHash = await bcrypt.hash(password, 10)
+        const { username, email, password } = req.body
+        if (email === '' || password === '') {
+            return res.status(400).send({
+              message: 'Enter email and password',
+              success: false,
+            })
+        }
         const user = {
             username,
             email,
-            password
+            password: await bcrypt.hash(password, 5),
         }
         await client.db("bookbazaar").collection("user").insertOne(user)
-        res.status(201).send(user)
+        res.status(200).send(user)
     } catch(err) {
         res.send(err)
     }
 })
 
-app.post()
+// Login
+app.post('/api/login', async (req,res)=>{
+    try {
+        const { email, password } = req.body
+        if (email === '' || password === '') {
+            return res.status(400).send({
+                message: 'Enter email and password',
+                success: false,
+            })
+        }
+
+        const result = await client.db('bookbazaar').collection('user').findOne({ email: email })
+
+        if (result !== null && !(await bcrypt.compare(password, result.password))) {
+            return res.status(401).send({
+                message: 'Email or Password is incorrect',
+                success: false,
+            })
+        } else if (result === null) {
+            return res.status(404).send({
+                message: 'Email or Password is incorrect',
+                success: false,
+            })
+        } 
+        return res.status(200).send({
+            message: 'Login Success',
+            success: true,
+        })
+    } catch(err) {
+        res.send(err)
+    }
+})
