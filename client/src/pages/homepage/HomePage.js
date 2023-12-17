8aea5ec22012e7d76c3240c32941f5e57f837baf
Ariez <kritamet.cho@gmail.com>
2023-12-18 04:06:32 +0700

    update homepage

--- client/src/pages/homepage/HomePage.js
+++ client/src/pages/homepage/HomePage.js
@@ -2,11 +2,13 @@ import React from "react";
 import Showcase from "../../components/layouts/showcase/Showcase";
 import ProductList from "../../components/layouts/product list/ProductList";
 import Footer from "../../components/layouts/footer/Footer";
+import Slider from "../../components/layouts/slider/Slider";
 
 const HomePage = () => {
     return (
         <section>
             <Showcase />
+            <Slider />
             <ProductList />
             <Footer />
         </section>
