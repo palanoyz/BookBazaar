369b753089506d78f389d732d8c36f95a76da4d4
Ariez <kritamet.cho@gmail.com>
2023-12-19 03:51:30 +0700

    frontend final ver. ?

--- client/src/pages/signup page/Signup.js
+++ client/src/pages/signup page/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./signup.css";
 import AuthBgImg from "../../assets/auth-bg 2.jpg"
